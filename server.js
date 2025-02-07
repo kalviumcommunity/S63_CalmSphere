70334d6fea732ea556f53a3af360beaf557858ae
Bhoomi-18 <abhoomi20@gmail.com>
2025-02-07 11:00:37 +0530

    MongoDB connected

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const connectDB = require('./connectDB'); // Import connectDB function
 const app = express();
 
+// Middleware
 app.use(express.json());
 
+// Connect to MongoDB
+connectDB(); 
 
+// Routes
 app.get('/ping', (req, res) => {
   res.send('Pong!');
 });
 
-
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
